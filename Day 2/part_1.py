c7643c1bf9b8d43d0c0fa2b8d283ba323d754355
Max Van Raden <maxvanraden@gmail.com>
2022-12-02 19:22:24 -0800

    Part 1 Day 2

---
+++ Day 2/part_1.py
@@ -0,0 +1,40 @@
+# Read lines of letter pairs representing Rock-Paper-Scissors matches and calculate points based on results
+# X and A: Rock (+1 point)
+# Y and B: Paper (+2 points)
+# Z and C: Scissors (+3 points)
+# Winning is worth 6 points, drawing is worth 3 points
+
+
+infile = open('input.txt')
+
+lines = infile.readlines()
+points = 0
+
+for line in lines:
+    try:
+        opponent = line[0]
+        player = line[2]
+    except BaseException as err:
+        print(f"Error occurred: {err}")
+        exit()
+    if player == 'X':
+        points += 1
+        if opponent == 'C':
+            points += 6
+        if opponent == 'A':
+            points += 3
+    elif player == 'Y':
+        points += 2
+        if opponent == 'A':
+            points += 6  
+        if opponent == 'B':
+            points += 3
+    elif player == 'Z':
+        points += 3
+        if opponent == 'B':
+            points += 6
+        elif opponent == "C":
+            points += 3
+print(f"Total number of points won: {points}. Total matches played: {len(lines)}")
+
+
