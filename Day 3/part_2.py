63f3f6981950d312c529cabd60ec8e42afe9b108
Max Van Raden <vanraden@pdx.edu>
2022-12-03 10:17:30 -0800

    Day 3 Part 2 done

--- Day 3/part_2.py
+++ Day 3/part_2.py
@@ -1,2 +1,19 @@
-from part_1 import char_range, lines, priorities
+from part_1 import lines, priorities
 
+total = 0
+
+for i in range(0, len(lines), 3):
+    backpack_1 = {}
+    backpack_2 = {}
+
+    for item in lines[i]:
+        backpack_1[item] = 1
+    for item in lines[i+1]:
+        backpack_2[item] = 1
+    for item in lines[i+2]:
+        if item in backpack_1.keys() and item in backpack_2.keys():
+            print(f"Badge found: {item}")
+            total += priorities[item]
+            backpack_1.clear()
+            backpack_2.clear()
+print(total)
